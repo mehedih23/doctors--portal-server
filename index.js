6f45367e1db8d27022d9699a48b3efdbbd93a870
mehedih23 <nmehedi23@gmail.com>
2022-05-12 13:19:09 +0600

    polished

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal-cluster.tenaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
