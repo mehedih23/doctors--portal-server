9dc710d26b5a4ed04a36dca41f723137fc7c355f
mehedih23 <nmehedi23@gmail.com>
2022-05-16 20:51:46 +0600

    'polishing'

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { query } = require('express');
 
 app.use(cors());
 app.use(express.json());
